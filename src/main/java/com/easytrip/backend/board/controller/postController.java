639d61868a4c327a255c2dd7848815a9a24b38c5
Ahn <youn_tip@naver.com>
2024-02-27 23:42:26 +0900

    .

--- src/main/java/com/easytrip/backend/board/controller/postController.java
+++ src/main/java/com/easytrip/backend/board/controller/postController.java
@@ -1,6 +1,7 @@
 package com.easytrip.backend.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,7 @@ public class postController {
      * @return
      */
     @GetMapping
-    public String board() {
+    public String board(Model model) {
 
 
 
