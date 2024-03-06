27c2c0fa4b6bd356b75dd67801dc0eac5044e2ff
Ahn <youn_tip@naver.com>
2024-03-06 22:10:18 +0900

    .

--- src/main/java/com/easytrip/backend/board/controller/BoardController.java
+++ src/main/java/com/easytrip/backend/board/controller/BoardController.java
@@ -1,98 +1,64 @@
 package com.easytrip.backend.board.controller;
 
-import com.easytrip.backend.board.dto.BoardDetailDto;
-import com.easytrip.backend.board.dto.BoardListDto;
-import com.easytrip.backend.board.dto.BoardRequestDto;
 import com.easytrip.backend.board.service.BoardService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/boards")
+@Controller
 public class BoardController {
 
     private BoardService boardService;
 
-
-    @PostMapping
-    public ResponseEntity<String> writePost(@Valid @RequestBody BoardRequestDto boardRequestDto, MultipartFile file) throws Exception {
-        String response = boardService.writePost(boardRequestDto, file);
-
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/{postId}")
-    public ResponseEntity<String> updatePost(@Valid @PathVariable(name = "boardId") Long boardId,
-                                             @RequestBody BoardRequestDto boardRequestDto, MultipartFile file) throws Exception {
-
-        String response = boardService.updatePost(boardId, boardRequestDto, file);
-
-        return ResponseEntity.ok(response);
-    }
-
-
-    @DeleteMapping("/{postId}")
-    public ResponseEntity<String> deletePost(@PathVariable(name = "boardId") Long boardId) {
-        String response = boardService.deletePost(boardId);
-
-        return ResponseEntity.ok(response);
-
-    }
-
-    @GetMapping("/lists/{postId}")
-    public ResponseEntity<List<BoardListDto>> getList(@RequestParam(name = "sortByLikes", defaultValue = "false")
-                                                      Boolean sortByLikes) {
-
-        List<BoardListDto> response = boardService.getList(sortByLikes);
-
-        return ResponseEntity.ok(response);
-
-    }
-
-    @GetMapping("/{postId}")
-    public ResponseEntity<BoardDetailDto> getDetail(@PathVariable(name = "boardId") Long boardId) {
-
-        BoardDetailDto response = boardService.getDetail(boardId);
-
-        return ResponseEntity.ok(response);
-
-    }
-
-    @GetMapping("/my-posts")
-    public ResponseEntity<List<BoardListDto>> getMyPost() {
-        List<BoardListDto> response = boardService.getMyPost();
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/{postId}/like")
-    public void postLike(@PathVariable(name = "boardId") Long boardId) {
-
-        boardService.likes(boardId);
+    /**
+     *  게시물 목록
+     * @param model
+     * @param pageable
+     * @return
+     */
+    @GetMapping({"", "/"})
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        model.addAttribute("boards", boardService.getPostList(pageable));
+        return "index";
     }
 
     /**
-     * 게시글 검색
-     * @param keyword
-     * @param searchOption
+     * 게시물 저장
      * @return
      */
-    @GetMapping("/search")
-    public ResponseEntity<List<BoardListDto>> search(
-            @Valid @NotNull(message = "검색어를 입력해주세요.")
-            @RequestParam(name = "keyword") String keyword,
-            @RequestParam(name = "searchOption") String searchOption) {
+    @GetMapping("/board/saveForm")
+    public String saveForm() { return "board/saveForm";}
 
-        List<BoardListDto> response = boardService.search(keyword, searchOption);
-        return ResponseEntity.ok(response);
+    /**
+     * 게시물 확인
+     * @param model
+     * @param id
+     * @return
+     */
+    @GetMapping("/board/{id}")
+    public String getPost(Model model, @PathVariable int id) {
+        model.addAttribute("boards", boardService.getPost(id));
 
+        return "board/detail";
     }
 
+    /**
+     * 게시물 수정
+     * @param model
+     * @param id
+     * @return
+     */
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(Model model, @PathVariable int id) {
+        model.addAttribute("boards", boardService.getPost(id));
+
+        return "board/updateForm";
+
+    }
 }
--- src/main/java/com/easytrip/backend/board/domain/BoardEntity.java
+++ src/main/java/com/easytrip/backend/board/domain/BoardEntity.java
@@ -1,7 +1,6 @@
 package com.easytrip.backend.board.domain;
 
 import com.easytrip.backend.member.domain.MemberEntity;
-import com.easytrip.backend.type.BoardStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,47 +9,36 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder(toBuilder = true)
-@Table(name = "board")
+@Builder
+@Table(name = "boards")
 public class BoardEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long boardId;
+    private long id;
 
     @Column(nullable = false, length = 100)
     private String title;
 
-    private String nickname;
-
-    private String fileName;
-
-    private String filePath;
-
-//    @Lob
+    @Lob
     private String content;
+
+    @Column(nullable = false, length = 20)
+    private String category;
 
     @ManyToOne(fetch = FetchType.LAZY) // Many = Board, One = Member
     @JoinColumn(name = "memberId")
-    private MemberEntity memberId; // DB는 오브젝트를 저장할 수 있다. FK, 자바는 오브젝트를 저장 할 수 있다.
+    private MemberEntity member; // DB는 오브젝트를 저장할 수 있다. FK, 자바는 오브젝트를 저장 할 수 있다.
 
+    @CreationTimestamp
+    private Timestamp createDate;
 
-    private Integer likeCnt;
-
-    private LocalDateTime createDate;
-
-    private LocalDateTime modDate;
-
-    private LocalDateTime deleteDate;
-
-    @Enumerated(EnumType.STRING)
-    private BoardStatus status;
+    private String status;
 
 
 
