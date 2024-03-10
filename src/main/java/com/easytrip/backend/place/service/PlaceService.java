27f18b255824628195c910e06ef17d9875c935c5
itpark1018 <itpark1018@gmail.com>
2024-03-10 23:01:17 +0900

    (feat:placeList)
    지역과 카테고리를 선택하면 그에 해당하는 지역 정보가 리스트로 제공되는 기능을 구현했습니다.

--- src/main/java/com/easytrip/backend/place/service/PlaceService.java
+++ src/main/java/com/easytrip/backend/place/service/PlaceService.java
@@ -3,6 +3,7 @@ package com.easytrip.backend.place.service;
 import com.easytrip.backend.place.dto.MapDto;
 import com.easytrip.backend.place.dto.PlaceDto;
 import com.easytrip.backend.place.dto.request.PlaceRequest;
+import com.easytrip.backend.type.PlaceCategory;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +15,6 @@ public interface PlaceService {
   PlaceDto getInfo(String accessToken, Long placeId);
 
   List<MapDto> getMapData(String accessToken, Double x, Double y);
+
+  List<PlaceDto> getList(String accessToken, String state, PlaceCategory category);
 }
