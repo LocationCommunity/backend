8a8b8edc11e1f5a884d6cdf40e6fa58b2743c8ef
Ahn <youn_tip@naver.com>
2024-03-26 21:04:00 +0900

    파일 업로드

--- src/main/java/com/easytrip/backend/board/service/BoardService.java
+++ src/main/java/com/easytrip/backend/board/service/BoardService.java
@@ -1,10 +1,8 @@
 package com.easytrip.backend.board.service;
 
-import com.easytrip.backend.board.dto.BoardDetailDto;
-import com.easytrip.backend.board.dto.BoardListDto;
-import com.easytrip.backend.board.dto.BoardRequestDto;
+import com.easytrip.backend.board.dto.*;
 
-import com.easytrip.backend.board.dto.PostPlaceDto;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,9 +11,9 @@ import java.util.List;
 @Service
 public interface BoardService {
 
-    String writePost(BoardRequestDto boardRequestDto, MultipartFile file, PostPlaceDto postPlaceDto) throws Exception;
+    String writePost(BoardRequestDto boardRequestDto,BoardPlaceDto boardPlaceDto, List<MultipartFile> files) throws Exception;
 
-    String updatePost(Long boardId, BoardRequestDto boardRequestDto, MultipartFile file, PostPlaceDto postPlaceDto) throws Exception;
+    String updatePost(Long boardId, BoardRequestDto boardRequestDto, List<MultipartFile> files) throws Exception;
 
     String deletePost(Long boardId);
 
