6308ced44552247423c995ffb0338078d6b666c7
itpark1018 <itpark1018@gmail.com>
2024-03-29 00:28:17 +0900

    (feat:myShareUpdate)
    내가 공유한 장소의 정보를 수정하는 기능을 구현했습니다.

--- src/main/java/com/easytrip/backend/place/service/PlaceService.java
+++ src/main/java/com/easytrip/backend/place/service/PlaceService.java
@@ -3,7 +3,9 @@ package com.easytrip.backend.place.service;
 import com.easytrip.backend.place.dto.MapDto;
 import com.easytrip.backend.place.dto.PlaceDto;
 import com.easytrip.backend.place.dto.request.PlaceRequest;
+import com.easytrip.backend.place.dto.request.PlaceUpdateRequest;
 import com.easytrip.backend.type.PlaceCategory;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +16,9 @@ public interface PlaceService {
   void share(String accessToken, PlaceRequest placeRequest, List<MultipartFile> files);
 
   List<PlaceDto> getMyShare(String accessToken);
+
+  void myShareUpdate(String accessToken, Long placeId, @Valid PlaceUpdateRequest placeUpdateRequest,
+      List<MultipartFile> files);
 
   PlaceDto getInfo(String accessToken, Long placeId);
 
