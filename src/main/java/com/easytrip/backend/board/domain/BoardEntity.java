68c30a30f10dcfa3a9c32fce12c3b8c9a3dad6be
Ahn <youn_tip@naver.com>
2024-08-08 22:01:22 +0900

    style: Board

--- src/main/java/com/easytrip/backend/board/domain/BoardEntity.java
+++ src/main/java/com/easytrip/backend/board/domain/BoardEntity.java
@@ -8,10 +8,7 @@ import com.easytrip.backend.place.domain.PlaceEntity;
 import com.easytrip.backend.type.BoardStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 import java.time.LocalDate;
@@ -19,7 +16,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
